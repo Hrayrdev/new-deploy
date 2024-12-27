e28eab7007edca6707cb3609301f84bdd10a17b0
Hrayr <hrayrsargsyan3010@gmail.com>
2024-12-27 13:08:05 +0300

    update new 4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,9 @@
       <img style="height: 50vh; width: 60vh" alt="Vue logo" src="../assets/paracetamol.jpg">
       <HelloWorld msg="Парацетамол"/>
     </div>
+    <div style="padding-top: 20px" v-if="elen">
+      <HelloWorld msg="Тут должна быть твоя фотография Элен 💔"/>
+    </div>
 
   </div>
 </template>
@@ -23,15 +26,23 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 const inputValue = ref('')
 const lenor = ref(false)
 const paracetamol = ref(false)
+const elen = ref(false)
 
 function validFunc() {
   if (inputValue.value === '999') {
     lenor.value = false
+    elen.value = false
     paracetamol.value = true
   }
   if (inputValue.value === 'zqj') {
     paracetamol.value = false
+    elen.value = false
     lenor.value = true
+  }
+  if (inputValue.value === 'Любовь всей моей жизни') {
+    paracetamol.value = false
+    lenor.value = false
+    elen.value = true
   }
 }
 
